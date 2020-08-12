5dd18ca071ab56a823d9706c28f080ac85ed6c5e
nguyendinhthuan <nguyendinhthuan1698@gmail.com>
2020-08-12 11:16:18 +0700

    e

--- app/src/main/java/com/example/demotruyendulieucacactivity/MainActivity.java
+++ app/src/main/java/com/example/demotruyendulieucacactivity/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.demotruyendulieucacactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.app.Dialog;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -16,9 +20,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button_NhapThongTin, button_ChonSoThich;
+    private Button button_NhapThongTin, button_ChonSoThich, button_Xong;
     //private ListView listView;
     private TextView ten, diachi;
+    private CheckBox checkBox_1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
         //listView = (ListView) findViewById(R.id.listView);
         ten = (TextView) findViewById(R.id.textView_ten);
         diachi = (TextView) findViewById(R.id.textView_diachi);
+
+
     }
 
     public void XuLy() {
@@ -46,6 +54,35 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, Main2Activity.class);
                 startActivity(intent);
+            }
+        });
+
+        button_ChonSoThich.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Dialog dialog = new Dialog(MainActivity.this);
+                dialog.requestWindowFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
+                dialog.setContentView(R.layout.dialog_sothich);
+                dialog.getWindow().setLayout(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT);
+                dialog.show();
+
+                button_Xong = (Button) dialog.findViewById(R.id.button_Xong);
+                checkBox_1 = (CheckBox) dialog.findViewById(R.id.checkBox_1);
+
+                button_Xong.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (checkBox_1.isChecked()) {
+                            String sothich;
+                            sothich = checkBox_1.getText().toString();
+                            ten.setText(sothich);
+//                    Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+//                    intent.putExtra("sothich", sothich);
+//                    startActivity(intent);
+                        }
+                        dialog.dismiss();
+                    }
+                });
             }
         });
     }
